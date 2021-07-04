759b46bd6ec9f0c46cba9785cd9184c4b887c266
Mujahid Islam <mislam@vfairs.com>
2021-07-05 02:28:36 +0500

    added logout url to protected routes

--- routes/api.php
+++ routes/api.php
@@ -28,12 +28,12 @@ Route::middleware('auth:sanctum')->get('/authenticated', function () {
 //public routes start
 Route::post('/register', [RegisterController::class, 'store']);
 Route::post('/login', [LoginController::class, 'validateLogin']);
-Route::get('/logout', [LoginController::class, 'logout']);
 Route::post('/verify-account', [RegisterController::class, 'verifyAccount']);
 //public routes end
 
 //protected routes start
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::resource('todo', ToDoController::class);
+    Route::get('/logout', [LoginController::class, 'logout']);
 });
 //protected routes end
